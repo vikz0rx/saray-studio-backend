b47417842761ca740e3842c7eaedfeffefe2c495
Viktor Zagoruyko <vik.zgrk@gmail.com>
2019-04-10 15:06:05 +0500

    Custom User

--- SarayAdmin/SarayAdmin/settings.py
+++ SarayAdmin/SarayAdmin/settings.py
@@ -5,6 +5,8 @@ SECRET_KEY = '3r)4p_)w@dr&3^#o)oj_7(n(6hw5s-q8qlz+i^mzkjoyd%r1%k'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
+AUTH_USER_MODEL = 'main.User'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -12,10 +14,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
-    'rest_framework',
     'main',
     'api_v0',
+    'corsheaders',
+    'rest_framework',
+    'mediumeditor',
 ]
 
 MIDDLEWARE = [
@@ -75,6 +78,21 @@ CORS_ORIGIN_WHITELIST = ('*', )
 CORS_ALLOW_METHODS = ('DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT', )
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
+
+MEDIUM_EDITOR_THEME = 'beagle'
+MEDIUM_EDITOR_OPTIONS = {
+    'toolbar': {
+        'static': True,
+        'buttons': ['bold', 'italic', 'underline', 'h2', 'h4', 'h6',]
+    },
+    'paste': {
+        'forcePlainText': True,
+        'cleanPastedHTML': False,
+        'cleanReplacements': [],
+        'cleanAttrs': ['class', 'style', 'dir',],
+        'cleanTags': ['meta',]
+    }
+}
 
 LANGUAGE_CODE = 'ru-RU'
 TIME_ZONE = 'Asia/Yekaterinburg'
--- SarayAdmin/main/admin.py
+++ SarayAdmin/main/admin.py
@@ -1,3 +1,31 @@
+from datetime import datetime
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'firstname', 'lastname', 'email', )
+
+    def get_exclude(self, request, obj=None):
+        exclude = ()
+        
+        if obj:
+            exclude = ('groups', 'user_permissions', )
+            if request.user == obj:
+                exclude += ()
+            else:
+                exclude += ('password', )
+
+        return exclude
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = ()
+
+        if obj:
+            readonly_fields += ('last_login', )
+            if request.user == obj:
+                readonly_fields += ('is_active', 'is_staff', 'is_superuser', )
+            elif request.user.is_superuser:
+                readonly_fields += ('username', 'firstname', 'lastname', 'email', 'is_staff', 'is_superuser', )
+
+        return readonly_fields
